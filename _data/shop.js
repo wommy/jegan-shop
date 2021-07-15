cacf84dde36fe5f3b1f55e3784e42a495ba4912e
wommy <tom@technomad.media>
2021-07-15 20:51:07 +0000

    feat: added data

---
+++ _data/shop.js
@@ -0,0 +1,219 @@
+module.exports = [
+{
+	img: "",
+	product: "Bondage Bear Temporary Tattoo",
+	price: 8,
+},{
+	img: "8B191E0C-3B4E-4F9A-94AB-9698AD64F82C",
+	product: "Shoe Lighter",
+	price: 6,
+},{
+	img: "2FF6F604-B9A3-4253-B616-AC1E8B8D49D00AF13CC6-1F0A-4F3D-84C4-874769D23436",
+	product: "Cow Lighter",
+	price: 6,
+},{
+	img: "Custom Lighter Case + Matching Art LighterIMG_5507.jpg",
+	product: "Custom Lighter Case + Matching Art Lighter",
+	price: 3,
+},{
+	img: "EF2A87B4-187F-47C4-9624-ECF9306124AA",
+	product: "Lava Shroom Lighter",
+	price: 6,
+},{
+	img: "Tattoo DesignLayne.png",
+	product: "Tattoo Design",
+	price: 2,
+},{
+	img: "Lighter Case + Matching Art LighterIMG_4711 copy.jpg",
+	product: "Lighter Case + Matching Art Lighter",
+	price: 2,
+},{
+	img: "Custom Nude LighterIMG_3580.jpg",
+	product: "Custom Nude Lighter",
+	price: 1,
+},{
+	img: "F0358AE2-DCB4-47D5-810A-79E12175E5E8",
+	product: "3 Mushroom Lighters + Sticker Set",
+	price: 2,
+},{
+	img: "Lighter WrapIMG_2773.jpg",
+	product: "Lighter Wrap",
+	price: 3,
+},{
+	img: "My Plants Wear Leather LighterIMG_4420.jpg",
+	product: "My Plants Wear Leather Lighter",
+	price: 6,
+},{
+	img: "Mushroom Keychain",
+	product: "Mushroom Keychain",
+	price: 1,
+},{
+	img: "11x17 Art PrintALL.jpg",
+	product: "11x17 Art Print",
+	price: 2,
+},{
+	img: "DC0765F7-873B-4836-A369-18368E3B5C5D",
+	product: "Deadly Flutter Sticker",
+	price: 2,
+},{
+	img: "Custom Pet PortraitUntitled-20-02.jpg",
+	product: "Custom Pet Portrait",
+	price: 3,
+},{
+	img: "Custom Nude PortraitHaley K-01.jpg",
+	product: "Custom Nude Portrait",
+	price: 3,
+},{
+	img: "4x6 Art PrintALL.jpg",
+	product: "4x6 Art Print",
+	price: 1,
+},{
+	img: "8.5x11 Art PrintALL.jpg",
+	product: "8.5x11 Art Print",
+	price: 1,
+},{
+	img: "Art CandleCANDLE#.jpg",
+	product: "Art Candle",
+	price: 3,
+},{
+	img: "7BDC0EF6-A54A-497A-B8C1-34AC7100C173",
+	product: "2 Mini Holographic Cumfort Me Stickers",
+	price: 2,
+},{
+	img: "FF968748-FBE1-45EA-81A1-F0A188937DC2",
+	product: "My Plants Wear Leather Sticker",
+	price: 2,
+},{
+	img: "F4286394-63B5-4A44-8C71-D740E825620F",
+	product: "Live Deliciously Sticker",
+	price: 2,
+},{
+	img: "92D7F9A5-A65F-459C-B18D-67BBB465517D",
+	product: "Deadly Flutter Pin",
+	price: 8,
+},{
+	img: "Deadly Flutter Patch",
+	product: "Deadly Flutter Patch",
+	price: 8,
+},{
+	img: "B462E6C1-7799-4E79-A5ED-ABD20170DCAA",
+	product: "Holographic Cassette Tape Sticker",
+	price: 2,
+},{
+	img: "4417C9A6-730A-437D-A72C-98C35AA13731",
+	product: "Skull Plant Sticker",
+	price: 2,
+},{
+	img: "Custom Art PortraitCody-01.jpg",
+	product: "Custom Art Portrait",
+	price: 3,
+},{
+	img: "Lust Lighter",
+	product: "Lust Lighter",
+	price: 6,
+},{
+	img: "Don't Be A Dick Lighter",
+	product: "Don't Be A Dick Lighter",
+	price: 6,
+},{
+	img: "Custom Portrait LighterIMG_3215.jpg",
+	product: "Custom Portrait Lighter",
+	price: 1,
+},{
+	img: "IMG_4592.jpgIMG_4608.jpg",
+	product: "Zodiac Lighter",
+	price: 6,
+},{
+	img: "9B57CD79-B387-4B86-B4F5-57EE919CACE2",
+	product: "2 Pack MYSTERY Lighters",
+	price: 6,
+},{
+	img: "Led Zeppelin Lighter",
+	product: "Led Zeppelin Lighter",
+	price: 6,
+},{
+	img: "5A1BD734-E55B-4263-B34F-BC9988BC0828",
+	product: "Rainbow Penis Lighter",
+	price: 6,
+},{
+	img: "Skull Cone Lighter",
+	product: "Skull Cone Lighter",
+	price: 6,
+},{
+	img: "Horny Lighter",
+	product: "Horny Lighter",
+	price: 6,
+},{
+	img: "Praise Me Lighter",
+	product: "Praise Me Lighter",
+	price: 6,
+},{
+	img: "Play With Me Lighter",
+	product: "Play With Me Lighter",
+	price: 6,
+},{
+	img: "Devil Inside Lighter",
+	product: "Devil Inside Lighter",
+	price: 6,
+},{
+	img: "Vibin' Lighter",
+	product: "Vibin' Lighter",
+	price: 6,
+},{
+	img: "Crown of Death Lighter",
+	product: "Crown of Death Lighter",
+	price: 6,
+},{
+	img: "Dead Inside Lighter",
+	product: "Dead Inside Lighter",
+	price: 6,
+},{
+	img: "Skull Flame Lighter",
+	product: "Skull Flame Lighter",
+	price: 6,
+},{
+	img: "Cumfort Me Lighter",
+	product: "Cumfort Me Lighter",
+	price: 6,
+},{
+	img: "Live Deliciously Lighter",
+	product: "Live Deliciously Lighter",
+	price: 6,
+},{
+	img: "Lit Lighter",
+	product: "Lit Lighter",
+	price: 6,
+},{
+	img: "Be Nice or Leaf Lighter",
+	product: "Be Nice or Leaf Lighter",
+	price: 6,
+},{
+	img: "Spooky Smoke Lighter",
+	product: "Spooky Smoke Lighter",
+	price: 6,
+},{
+	img: "Flowers Grow Lighter",
+	product: "Flowers Grow Lighter",
+	price: 6,
+},{
+	img: "Witchy Crystal Lighter",
+	product: "Witchy Crystal Lighter",
+	price: 6,
+},{
+	img: "Deadly Flutter Lighter",
+	product: "Deadly Flutter Lighter",
+	price: 6,
+},{
+	img: "Light Me Up Lighter",
+	product: "Light Me Up Lighter",
+	price: 6,
+},{
+	img: "Skull Plant Lighter6685A7DE-EB19-4E7E-ADD2-DFCE0BE0BC35",
+	product: "Skull Plant Lighter",
+	price: 6,
+},{
+	img: "IMG_5005.jpg",
+	product: "Mushroom Lighter",
+	price: 6,
+},
+]
